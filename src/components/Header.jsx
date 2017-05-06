578a0d35033d47f42e160224c74b46324498be59
839 <8398a7@gmail.com>
2017-05-06 16:02:08 +0900

    headerを改良

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import profile from '../images/profile.jpg';
 import './Header.scss';
+import profile from '../images/profile.jpg';
+import Navbar from './Navbar';
 
 const Header = () => (
   <header>
     <img className="circle responsive-img" src={profile} />
     <h1><a href="https://twitter.com/8398a7" style={{color: 'white'}}>@8398a7</a></h1>
+    <p className="main">Web Developer</p>
+    <div />
+    <p>aws</p>
+    <p>rails</p>
+    <p>chef</p>
+    <p>docker</p>
+    <p>react</p>
+    <Navbar />
   </header>
 )
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.scss';
+
+const Navbar = () => (
+  <nav className="blue lighten-4">
+    <div className="navbar-wrapper">
+      <ul>
+        <li>about me</li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Navbar;
