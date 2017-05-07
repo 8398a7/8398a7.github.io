f3f5c2f43e2028080d0c37c29915a5f0c97b7ba2
839 <8398a7@gmail.com>
2017-05-07 22:45:51 +0900

    [#7] store, historyを引き継いでHMR可能、設定をwebpack1に変更

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import createHistory from 'history/createBrowserHistory';
+import createStore from './store';
 import App from './App';
 
-const render = (Component) => {
+const history = createHistory();
+const store = createStore(history);
+
+const render = (Component, store, history) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Component {...{ store, history }} />
     </AppContainer>,
     document.getElementById('app'),
   );
 };
 
-render(App);
+render(App, store, history);
 
 if (module.hot) {
-  module.hot.accept('./App', () => { render(App); });
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp, store, history);
+  });
 }
