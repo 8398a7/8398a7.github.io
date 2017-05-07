096bd8cff6b05f2165394eafba505dbed1bee73e
839 <8398a7@gmail.com>
2017-05-07 18:07:06 +0900

    [#7] hotloader経由で更新するように修正

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-render(
-  <App />,
-  document.getElementById('app'),
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app'),
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => { render(App); });
+}
