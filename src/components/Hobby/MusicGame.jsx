0ae34febd20b472887a3ffd506c9aec4e15e622d
839 <8398a7@gmail.com>
2017-05-27 19:28:16 +0900

    [#22] flexboxでMusicGameコンポーネントの2カラムを実現

--- src/components/Hobby/MusicGame.jsx
+++ src/components/Hobby/MusicGame.jsx
@@ -6,30 +6,32 @@ import SectionTitle from '../SectionTitle';
 const MusicGame = () => (
   <div className="col s12">
     <SectionTitle {...{ icon: 'gamepad', title: 'Music Game(2013 - )' }} />
-    <div className="col s6">
-      <Player><source src="https://husq.tk/videos/bms1.mp4" /></Player>
-    </div>
-    <div className="col s6">
-      <table className="striped">
-        <tbody>
-          <tr>
-            <td>iidx</td>
-            <td>☆☆</td>
-          </tr>
-          <tr>
-            <td>bms</td>
-            <td>★7</td>
-          </tr>
-          <tr>
-            <td>recommend</td>
-            <td>★18.21</td>
-          </tr>
-          <tr>
-            <td>style</td>
-            <td>SP/2P/対象固定+3:5</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="flexbox-main">
+      <div className="flexbox-column">
+        <Player><source src="https://husq.tk/videos/bms1.mp4" /></Player>
+      </div>
+      <div className="flexbox-column">
+        <table className="striped">
+          <tbody>
+            <tr>
+              <td>iidx</td>
+              <td>☆☆</td>
+            </tr>
+            <tr>
+              <td>bms</td>
+              <td>★7</td>
+            </tr>
+            <tr>
+              <td>recommend</td>
+              <td>★18.21</td>
+            </tr>
+            <tr>
+              <td>style</td>
+              <td>SP/2P/対象固定+3:5</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 );
