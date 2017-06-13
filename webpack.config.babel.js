5679d08d93f72cf1a91c3a2d485536de99738b6e
839 <8398a7@gmail.com>
2017-06-14 01:43:42 +0900

    fix eslint

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -40,7 +40,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(otf|woff|svg|ttf|eot)([\?]?.*)$/,
+        test: /\.(otf|woff|woff2|svg|ttf|eot)?$/,
         use: ['base64-font-loader'],
       },
       {
