518809495e69a26d1840ad6c7f48d8e00d56eb48
839 <8398a7@gmail.com>
2019-10-22 00:00:56 +0900

    [#54] ナビバーの状態をreduxで管理

---
+++ src/ducks/UI.ts
@@ -0,0 +1,16 @@
+import ActionReducer from 'action-reducer';
+import produce from 'immer';
+
+const initialState = {
+  openNavbar: false,
+};
+const { createAction, reducer } = ActionReducer(initialState);
+export default reducer;
+
+export const actions = {
+  toggleNavbar: createAction('ui/toggleNavbar', state =>
+    produce(state, draft => {
+      draft.openNavbar = !draft.openNavbar;
+    }),
+  ),
+};
