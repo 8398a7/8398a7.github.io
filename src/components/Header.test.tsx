ecc445a3f2db2064e5e33eb693200d51b52d4eae
839 <8398a7@gmail.com>
2025-09-19 01:39:25 +0900

    testが通るように修正

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
 import { MemoryRouter } from 'react-router';
 import Header from './Header';
+import ui from '../ducks/UI';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const store = createStore(
+    combineReducers({
+      ui,
+    }),
+  );
   render(
-    <MemoryRouter initialEntries={['/']}>
-      <Header />
-    </MemoryRouter>,
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/']}>
+        <Header />
+      </MemoryRouter>
+    </Provider>,
     div,
   );
   unmountComponentAtNode(div);
--- src/lib/api.test.ts
+++ src/lib/api.test.ts
@@ -1,7 +1,32 @@
+import axios from 'axios';
 import API from './api';
 
+jest.mock('axios');
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+beforeEach(() => {
+  mockedAxios.get.mockReset();
+});
+
 it('.fetchAbilitysheetUsers', async () => {
+  mockedAxios.get.mockResolvedValueOnce({ data: { users: 42 } });
+
   const res = await API.fetchAbilitysheetUsers();
-  expect(res).toHaveProperty('users');
-  expect(typeof res.users).toBe('number');
+
+  expect(mockedAxios.get).toHaveBeenCalledWith(
+    'https://api-sp12.iidx.app/users/count',
+  );
+  expect(res).toEqual({ users: 42 });
+});
+
+it('.fetchIstUsers', async () => {
+  mockedAxios.get.mockResolvedValueOnce({ data: { users: 7 } });
+
+  const res = await API.fetchIstUsers();
+
+  expect(mockedAxios.get).toHaveBeenCalledWith(
+    'https://score.iidx.app/api/v1/users/count',
+  );
+  expect(res).toEqual({ users: 7 });
 });
