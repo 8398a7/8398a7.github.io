e7f0c7d1e967dd65676fad61d8293fd28625f52a
839 <8398a7@gmail.com>
2026-03-18 22:38:41 +0900

    Update YouTube video source in Gymkhana component

--- src/components/Hobby/Gymkhana.tsx
+++ src/components/Hobby/Gymkhana.tsx
@@ -69,7 +69,7 @@ const Gymkhana: FC = () => (
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/dB9-RR_M0vc"
+            src="https://www.youtube.com/embed/kVU7eegjHY8"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
